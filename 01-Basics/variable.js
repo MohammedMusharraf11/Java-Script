360e9f19aa3e5be3588a97bd955a6f0d6bde1064
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-01 10:22:42 +0000

    Basics

--- 01-Basics/variable.js
+++ 01-Basics/variable.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+let accountMail = 'hi@gmail.com'
+const accountId = 12345
+var accountCity = 'Bengaluru'
+accountState = 'Karnataka'
+console.table([accountId,accountState,accountCity,accountMail])
