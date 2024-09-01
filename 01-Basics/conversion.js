fd044809368fefa1be3867c17fad9d0b6120631d
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-01 12:52:09 +0000

    Conversion of Data Types

---
+++ 01-Basics/conversion.js
@@ -0,0 +1,18 @@
+let score = 33
+
+console.log(typeof(score)); //Here score is number
+
+let scoreInString = "33"
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber);
+
+// Convrsion to Number 
+// "33" => 33
+//  "33abc" => NaN
+//  true => 1
+//  false => 0
+
+//  Boolean Conversion
+// 1=> true ; 0 => false
+//  "" => false
+
