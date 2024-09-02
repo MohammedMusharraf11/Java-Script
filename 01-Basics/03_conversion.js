61d3c080955b842ea8e3f31849a4699c2425f5ba
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-02 13:03:51 +0000

    Renamed Files

---
+++ 01-Basics/03_conversion.js
@@ -0,0 +1,37 @@
+let score = 33
+
+console.log(typeof(score)); //Here score is number
+
+let scoreInString = "33"
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber);
+
+// Convrsion to Number 
+// "33" => 33
+//  "33abc" => NaN
+//  true => 1
+//  false => 0
+
+//  Boolean Conversion
+// 1=> true ; 0 => false
+//  "" => false
+
+// *********** Operations ***********
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //132
+
+console.log(+true); //1
+console.log(+""); // 0
+
+// Increment Operator
+
+// x++ {PostFix Operator} 
+//  Returns the value before Incrementing
+
+// ++x {PreFix Operator}
+//  Returns the value after Incrementing
+
+
