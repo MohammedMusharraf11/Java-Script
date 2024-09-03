00fc70d4b13e345acabbdb319c4f1d96af51cb8e
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-03 12:48:47 +0000

    Added Math and Num

---
+++ 01-Basics/07_nums&variables.js
@@ -0,0 +1,42 @@
+const score = 400
+console.log(score);
+
+
+
+const balance = new Number(100)
+console.log(balance);
+
+const otherNumber = 123.896
+
+console.log(otherNumber.toPrecision(4));
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-IN'));
+
+// ********** Math *******
+
+console.log(Math);
+
+console.log(Math.abs(-18));
+
+console.log(Math.round(4.6));
+
+// console.log(Math.max(1,10,65,43));
+
+// console.log(Math.min(-5,10,3,2));
+
+console.log(Math.random()); //Gives Values b/w 0 & 1
+
+console.log((Math.random()*10)+1); // Values above 1 
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
+
+
+
+
+
+
+
