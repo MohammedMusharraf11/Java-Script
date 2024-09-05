e55e06ba33306eee0676e784063bcee84c49d946
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-05 11:18:18 +0000

    Objects!!

---
+++ 02-Arrays/04-objects.js
@@ -0,0 +1,28 @@
+const tinderUser = new Object()
+
+tinderUser.id = "PES2CS915"
+tinderUser.name = "John Doe"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const obj1 = {1:'a',2: 'b'}
+const obj2 = {1:'a',2: 'b'}
+
+const mergedObj = Object.assign({},obj1,obj2)
+
+console.log(mergedObj);
+
+// Same thing can be done using spread!!
+
+const obj3 = {
+    ...obj1,...obj2
+}
+
+console.log(obj3);
+
+console.log(Object.keys(tinderUser)); // --> Returns array which contains keys
+console.log(Object.values(tinderUser));
+
+// console.log(Object.entries(tinderUser));
+
